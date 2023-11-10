52eb75ded9a154331be7099c1e97a6e53ce85ad0
Young <youngheesomemoto@gmail.com>
2023-11-09 23:15:56 -0600

    copy paste

--- index.js
+++ index.js
@@ -1,120 +1,121 @@
-const EVENTS_URI = "https://fsa-crud-2aa9294fe819.herokuapp.com/api/2308-ACC-PT-WEB-PT-A/events";
+const EVENTS_URI =
+  "https://fsa-crud-2aa9294fe819.herokuapp.com/api/2308-ACC-PT-WEB-PT-A/events";
 
 const state = {
-    events: [],
-}
+  events: [],
+};
 
-const partiesList = document.querySelector('#display-parties');
+const partiesList = document.querySelector("#display-parties");
 
-//GET EVENT
-
+//GET EVENTS
 const getParties = async () => {
-    try {
-        const response = await fetch(EVENTS_URI);
-        const json = await response.json();
-        const parties = json.data;
-        if (json.error) {
-            throw new Error(json.error);
-        }
-        state.events = parties;
-    } catch (error) {
-        console.log(error)
+  try {
+    const response = await fetch(EVENTS_URI);
+    const json = await response.json();
+    const parties = json.data;
+    console.log("inside GET");
+    console.log(parties);
+    if (json.error) {
+      throw new Error(json.error);
     }
-    //get doesn't need a render.
+    state.events = parties;
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 //POST EVENT
-
+//name, time, location, description
 const createParty = async (name, description, unformattedDate, location) => {
-    try {
-        const date = new Date(unformattedDate);
-        const response = await fetch(EVENTS_URI, {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ name, description, date, location }),
-        });
-        const json = await response.json();
-        if (json.error) {
-            throw new Error(json.error);
-        }
-        init();
-    } catch (error) {
-        console.error(error)
+  try {
+    const date = new Date(unformattedDate);
+    const response = await fetch(EVENTS_URI, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ name, description, date, location }),
+    });
+    const json = await response.json();
+    if (json.error) {
+      throw new Error(json.error);
     }
-}
+    init();
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 document.querySelector("form").addEventListener("submit", (evt) => {
-    const formEl = evt.target;
-    evt.preventDefault();
-    createParty(
-        formEl.title.value,
-        formEl.description.value,
-        formEl.data.value,
-        formEl.location.value
-    );
+  const formEl = evt.target;
+  evt.preventDefault();
+  createParty(
+    formEl.title.value,
+    formEl.description.value,
+    formEl.date.value,
+    formEl.location.value
+  );
 
-    //clear inputs
-    formEl.title.value = "";
-    formEl.locaiton.value = "";
-    formEl.data.value = "";
-    formEl.location.value = "";
+  // clear inputs
+  formEl.title.value = "";
+  formEl.description.value = "";
+  formEl.date.value = "";
+  formEl.location.value = "";
 
-    //focus the first form element
-    formEl.title.focus();
+  // focus the first form element
+  formEl.title.focus();
 });
 
 //DELETE EVENT
 
 const deleteParty = async (id) => {
-    try {
-      const response = await fetch(EVENTS_URI + "/" + id, { method: "DELETE" });
-      const json = response.json();
-      const parties = json.data;
-      if (json.error) {
-        throw new Error(json.error);
-      }
-      state.events = parties;
-      init();
-    } catch (error) {
-      console.error(error);
+  try {
+    const response = await fetch(EVENTS_URI + "/" + id, { method: "DELETE" });
+    const json = response.json();
+    const parties = json.data;
+    if (json.error) {
+      throw new Error(json.error);
     }
-  };
+    state.events = parties;
+    init();
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 function renderEvents() {
-    if (!state.events.length || !state.events.length) {
-        partiesElement = document.createElement("div");
-        partiesElement.innerHTML = `<li>No events found</li>`;
-        return;
-    }
+  console.log("inside renderEvents");
+  console.log(state);
+  if (!state.events || !state.events.length) {
+    partiesElement = document.createElement("div");
+    partiesElement.innerHTML = `<li>No events found</li>`;
+    return;
+  }
 
-    const partyItems = state.events.map((party) => {
-        const partyItem = document.createElement("li");
-        partyItem.classList.add("party");
-        partyItem.innerHTML =
-            `
+  const partyItems = state.events.map((party) => {
+    console.log("inside partyItems");
+    console.log(party);
+    const partyItem = document.createElement("li");
+    partyItem.classList.add("party");
+    partyItem.innerHTML = `
         <h2>${party.name}</h2>
-        <p>${party.description}</p>
+        <p>${party.description}<p>
         <p>${party.date}</p>
         <p>${party.location}</p>
         <p>${party.id}</p>
         `;
 
-        const deleteButton = document.createElement("button");
-        deleteButton.textContent = "Delete Party";
-        partyItem.append(deleteButton);
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "Delete Party";
+    partyItem.append(deleteButton);
 
-        deleteButton.addEventListener("click", () => deleteParty(party.id));
-        return partyItem;
-    });
-    console.log(partyItems)
-    partiesList.replaceChildren(...partyItems); //calling and replacing. 
+    deleteButton.addEventListener("click", () => deleteParty(party.id));
+    return partyItem;
+  });
+  console.log(partyItems);
+  partiesList.replaceChildren(...partyItems);
 }
 
-const init = async () => { //initializing the function
-    await getParties();
-    renderEvents();
-
+const init = async () => {
+  await getParties();
+  renderEvents();
 };
-
-
-init(); //render
+init();
